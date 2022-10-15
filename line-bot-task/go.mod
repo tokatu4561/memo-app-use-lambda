0804b604f8d8281adf06354179a13d46f23fafc3
tokatu4561 <ryobsk4561@gmail.com>
2022-10-15 12:12:18 +0900

    lambdaからdynamodbにテスト接続

--- line-bot-task/go.mod
+++ line-bot-task/go.mod
@@ -1,8 +1,11 @@
-require github.com/aws/aws-lambda-go v1.23.0
+require (
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/aws/aws-sdk-go v1.44.114
+	github.com/guregu/dynamo v1.17.0
+)
 
 replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 
-module hello-world
+module line-bot-task
 
 go 1.16
-
--- line-bot-task/main.go
+++ line-bot-task/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
 )
 
 var (
@@ -21,27 +23,82 @@ var (
 	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
+// TODO: env管理する
+const AWS_REGION = "ap-northeast-1"
+const DYNAMO_ENDPOINT = "http://dynamodb:8000"
+
+type User struct {
+	UserID string `dynamo:"UserID,hash"`
+	Name   string `dynamo:"Name,range"`
+	Age    int    `dynamo:"Age"`
+	Text   string `dynamo:"Text"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+	// resp, err := http.Get(DefaultHTTPGetAddress)
+	// if err != nil {
+	// 	return events.APIGatewayProxyResponse{}, err
+	// }
+
+	// if resp.StatusCode != 200 {
+	// 	return events.APIGatewayProxyResponse{}, ErrNon200Response
+	// }
+
+	// ip, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return events.APIGatewayProxyResponse{}, err
+	// }
+
+	// if len(ip) == 0 {
+	// 	return events.APIGatewayProxyResponse{}, ErrNoIP
+	// }
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(AWS_REGION),
+		Endpoint:    aws.String(DYNAMO_ENDPOINT),
+		Credentials: credentials.NewStaticCredentials("dummy", "dummy", "dummy"),
+	})
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
 	}
 
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
+	db := dynamo.New(sess)
 
-	ip, err := ioutil.ReadAll(resp.Body)
+	db.Table("UserTable").DeleteTable().Run()
+
+	err = db.CreateTable("UserTable", User{}).Run()
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
+	}
+	table := db.Table("UserTable")
+
+	var user User
+
+	err = table.Put(&User{UserID: "1234", Name: "太郎", Age: 20}).Run()
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
 	}
 
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
+	err = table.Get("UserID", "1234").Range("Name", dynamo.Equal, "太郎").One(&user)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 500,
+		}, nil
 	}
+	fmt.Printf("GetDB%+v\n", user)
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       fmt.Sprintf("Hello, %v", string("hello")),
 		StatusCode: 200,
 	}, nil
 }
