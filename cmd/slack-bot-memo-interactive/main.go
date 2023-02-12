2ed8f2f0a9d692a44907f83361f847940f795a48
tokatu4561 <ryobsk4561@gmail.com>
2023-02-13 02:15:17 +0900

    memo 削除 動かないけど 一旦

--- cmd/slack-bot-memo-interactive/main.go
+++ cmd/slack-bot-memo-interactive/main.go
@@ -2,40 +2,61 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slackevents"
 )
 
+type Actions struct {
+	Name string `json:"name"`
+	Value string `json:"value"`
+	ActionType string `json:"type"`
+}
+
+type MessageActionPayload struct {
+	Actions []*Actions `json:"actions"`
+	CallbackId string `json:"callback_id"`
+	Channel struct {
+		Id string `json:"id"`
+		Name string `json:"name"`
+	} `json:"channel"`
+	User struct {
+		Id string `json:"id"`
+		Name string `json:"name"`
+	} `json:"User"`
+	ActionTs string `json:"action_ts"`
+	MessageTs string `json:"message_ts"`
+	AttachmentId string `json:"attachment_id"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// api := slack.New(os.Getenv("SLACK_OAUTH_TOKEN"))
+	slackApi := slack.New(os.Getenv("SLACK_OAUTH_TOKEN"))
 
-	err := Verify(ConvertHeaders(request.Headers), []byte(request.Body))
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: "slack conection error", StatusCode: 400}, err
+	// JSONを読み取る
+	type RequestPayload struct {
+		Payload MessageActionPayload `json:"payload"`
 	}
-	
-	body := request.Body
-	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-	if err != nil {
-		return events.APIGatewayProxyResponse{Body: "slack conection error", StatusCode: 500}, err
-	}
+	var payload RequestPayload
+	// readJson(request, &payload)
 
-	switch eventsAPIEvent.Type {
-		case slackevents.URLVerification:
-			res, err := HandleURLVerification(body)
-			if err != nil {
-				return events.APIGatewayProxyResponse{Body: "slack conection error", StatusCode: 500}, err
-			}
-			return events.APIGatewayProxyResponse{
-				Body:       res.Challenge,
-				StatusCode: 200,
-			}, nil
-		default:
+	_ = json.Unmarshal([]byte(request.Body), &payload)
+
+	fmt.Printf("Request body. %+v\n", request.Body)
+	log.Println(payload)
+
+	// 対象のメモを削除する
+	// ctl := di.NewSlackMemoController()
+	// ctl.DeleteMemo()
+
+	// 削除したことをslackのチャンネルに通知する
+	responseMsg := fmt.Sprintf("%sを追加しました!", "a")
+	_, _, err := slackApi.PostMessage(payload.Payload.Channel.Id, slack.MsgOptionText(responseMsg, false))
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: "bad request", StatusCode: 400}, err
 	}
 	
 	return events.APIGatewayProxyResponse{
@@ -44,33 +65,9 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}, nil
 }
 
-func ConvertHeaders(headers map[string]string) http.Header {
-    h := http.Header{}
-    for key, value := range headers {
-        h.Set(key, value)
-    }
-    return h
-}
-
-func HandleURLVerification(body string) (*slackevents.ChallengeResponse ,error) {
-	var res *slackevents.ChallengeResponse
-	if err := json.Unmarshal([]byte(body), &res); err != nil {
-		return nil, err
-	}
-
-	return res, nil
-}
-
-// slack からのリクエストかを検証 外部からのリクエストを受け付けないように
-// ヘッダー、body、Signing Secretで検証
-func Verify (header http.Header, body []byte) error {
-	verifier, err := slack.NewSecretsVerifier(header, os.Getenv("SLACK_SIGNING_SECRET"))
+func readJson(req events.APIGatewayProxyRequest, data interface{}) error {
+	err := json.Unmarshal([]byte(req.Body), &data)
 	if err != nil {
-		return err
-	}
-
-	verifier.Write(body)
-	if err := verifier.Ensure(); err != nil {
 		return err
 	}
 
